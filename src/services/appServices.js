8ea630bdfff63e6940364b4af0166875ed757a84
ansh-2609 <anshku037@gmail.com>
2026-02-14 18:16:07 +0530

    second commit

--- src/services/appServices.js
+++ src/services/appServices.js
@@ -1,5 +1,6 @@
 
-const API_URL = 'http://localhost:5000';
+
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 export const setSignupInfo = async (signupData) => {
   try {
